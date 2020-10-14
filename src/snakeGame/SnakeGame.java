fbb18f18d88dc4e3478ce8cbbe1e9b1e7fe08f32
precious-23 <68662724+precious-23@users.noreply.github.com>
2020-10-14 18:23:17 -0500

    Added setUpGame method and GraphicsGroup

--- src/snakeGame/SnakeGame.java
+++ src/snakeGame/SnakeGame.java
@@ -3,6 +3,7 @@ package snakeGame;
 import java.awt.Color;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
 
 public class SnakeGame {
     private static final int CANVAS_WIDTH = 700;
@@ -11,19 +12,42 @@ public class SnakeGame {
     private Mushroom mushroom;
     private BodyPart part;
     private Head head;
+    private GraphicsGroup group;
+
+
+    public SnakeGame() {
+        canvas = new CanvasWindow("Snake Game!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        setUpGame(); 
+    }
+
+
+    public void setUpGame() {
+        group = new GraphicsGroup();
+
+        mushroom = new Mushroom(50, 25, 30, 10, 400, 400);
+        group.add(mushroom);
+
+        part = new BodyPart(300, 300, Color.green);
+        group.add(part);
+
+        head = new Head(200, 200, Color.red);
+        group.add(head);
+
+        canvas.add(group);
+
+    }
+
 
     public static void main(String[] args) {
         SnakeGame snakeGame = new SnakeGame();
         
     }
 
-    public SnakeGame() {
-        canvas = new CanvasWindow("Snake Game!", CANVAS_WIDTH, CANVAS_HEIGHT);
-        mushroom = new Mushroom(50, 25, 30, 10, 400, 400);
-        canvas.add(mushroom);
-        part = new BodyPart(300, 300, Color.green);
-        canvas.add(part);
-        head = new Head(200, 200, Color.red);
-        canvas.add(head);
-    }
+
+
+
+
+
+
+
 }
